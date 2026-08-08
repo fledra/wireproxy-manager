93c2f832d4a3d956774a12ce4c121368b7f1c832
Fledra <baran@fledra.dev>
2026-08-08 15:29:34 +0300

    feat: added tauri-plugin-dialog and file selection

--- src/components/fields/FilePicker.vue
+++ src/components/fields/FilePicker.vue
@@ -1,12 +1,25 @@
 <template>
   <UFieldGroup>
     <UInput v-model="model" placeholder="No file selected" :ui="{ base: 'group-not-only:group-last:rounded-s-md' }" />
-    <UButton trailing-icon="i-lucide-file" variant="subtle" size="sm" class="whitespace-nowrap">
+    <UButton trailing-icon="i-lucide-file" variant="subtle" size="sm" class="whitespace-nowrap" @click="openDialog">
       Choose file
     </UButton>
   </UFieldGroup>
 </template>
 
 <script setup lang="ts">
+import { open } from '@tauri-apps/plugin-dialog';
+
 const model = defineModel<string>();
+
+async function openDialog() {
+  const path = await open({
+    multiple: false,
+    directory: false,
+  });
+
+  if (path) {
+    model.value = path;
+  }
+}
 </script>
